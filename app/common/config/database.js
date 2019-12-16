806c3397e3ac684c38c92a9866cb08ee8fd83df3
Victor.su <Victor.su@landasoft.com>
2019-12-16 18:01:16 +0800

    Initial commit

---
+++ app/common/config/database.js
@@ -0,0 +1,14 @@
+const mysql = require('think-model-mysql');
+
+module.exports = {
+    handle: mysql,
+    database: 'hiolabsDB',
+    prefix: 'hiolabs_',
+    encoding: 'utf8mb4',
+    host: 'as.bluelinkr.com',
+    port: '3306',
+    user: 'root',
+    password: 'Bluelinkr2019!',
+    dateStrings: true
+};
+//# sourceMappingURL=database.js.map
